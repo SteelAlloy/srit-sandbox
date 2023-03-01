49b595c01d840474678f3cac92993a9b39223db9
Steel <mael.acier@ensiie.fr>
2023-03-02 00:12:20 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import Srit from "srit";
+
+    import imageSrc from "./images/img.jpg";
+
+    const imageSizes = [100, 200, 400, 800];
+    const imageAlt = "Test Image";
+</script>
+
+<Srit src={imageSrc} sizes={imageSizes} alt={imageAlt} />
