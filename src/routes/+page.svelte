06ee9ccd4df871441bd0db90623adf2db01f03a2
Maël Acier <mael.acier@kleegroup.com>
2023-03-03 14:34:39 +0100

    change srit import

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
-    import Srit from "srit";
+    import {Srit} from "srit";
 
-    import imageSrc from "./images/img.jpg";
-
+    const imageSrc = "/images/img.jpg";
     const imageSizes = [100, 200, 400, 800];
     const imageAlt = "Test Image";
 </script>
